833ba734e58d325f0d0492fd2fe5bf4a219c9f9c
Eugen Lim Wei Kit <eugenlimwk@gmail.com>
2024-04-08 02:36:31 +0800

    random stuff to learn from website

--- src/App.js
+++ src/App.js
@@ -1,19 +1,93 @@
-import { useState } from "react";
-import {Button} from "./components/Button";
-import {Display} from "./components/Display"
+import React = require("react")
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
-  return (
-    <div>
-      <Button onClickFunction = {incrementCounter} increment={5}/>
-      <Button onClickFunction = {incrementCounter} increment={10}/>
-      <Button onClickFunction = {incrementCounter} increment={15}/>
-      <Button onClickFunction = {incrementCounter} increment={20}/>
-      <Display message={counter}/>
-    </div>
-  );
+const testData = [
+  {name: "Wayne", avatar_url: "website", company: "FaceBook"},
+  {name: "Wayne", avatar_url: "website", company: "FaceBook"},
+  {name: "Wayne", avatar_url: "website", company: "FaceBook"},
+]
+
+const CardList = (props) => {
+  <div>
+  {props.profile.map(profile => <Card key={profile.id} {...profile}/>)}
+    {/* <Card name = ""/>  //Above code can be used to sub here
+    <Card {...testData}/> */}
+  </div>
 }
 
-export default App;
+class Form extends React.Component {
+  // userNameInput = React.createRef();
+  state = {userName: ''}; // make this the value of the input element
+  handleSubmit = async (event) => {
+    event.preventDefault(); // must add whenever handle with forms to prevent default of refreshing
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    this.props.onSubmit(resp.data);
+    this.setState({userName: ''})
+    console.log(
+      // this.userNameInput.current.value
+    )
+  }
+  render() {
+    return ( //Set onChange to have React to reflect the change..
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.userName} onChange={event => this.setState({userName: event.target.value})} placeholder="GitHub username" required/>
+        <button>Add Card</button>
+      </form>
+    )
+  }
+}
+
+
+class Card extends React.Component{
+  render() {
+    // const profile = testData[0];
+    const profile = this.props;
+    return (
+      <div className="github-profile" style={{margin: '1rem'}}>
+        <img src={profile.avatar_url}/>
+        <div className="info">
+          <div className="name">{profile.name}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+class App extends React.Component{
+  // constructor(props) { //Constructor must be used when u want to access same property to more than 1 sibling
+  //   super(props);
+  //   this.state = {
+  //     profiles: testData,
+  //   };
+  // }
+  state = {
+    profiles:testData,
+  }
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile}/>
+        <CardList profiles={this.state.profiles}/>
+      </div>
+      
+    )
+  }
+}
+
+
+// function App() {
+  
+//   return (
+    
+//   );
+// }
+
+// export default App;
